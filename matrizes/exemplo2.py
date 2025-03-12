aa43df8d0a76f151ae22b486600717b79de7bac6
devpedrosena1 <devpedrosena1@gmail.com>
2025-03-12 09:41:03 -0300

    matrizes

---
+++ matrizes/exemplo2.py
@@ -0,0 +1,16 @@
+matriz = []
+
+i = 0
+while i < 4:
+    matriz.append([0] * 5)
+    i += 1
+
+num = 1
+
+for i in range(4):
+    for j in range(5):
+        matriz[i][j] = num
+        num += 1
+
+for linha in matriz:
+    print(linha)
